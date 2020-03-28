a58546e1de66bee9593146b65bfb9439138b3722
camilo <camilolagrava@gmail.com>
2020-03-28 11:38:50 -0400

    primero de correccion de errores de databse

--- WorkAnt/FuncionesBasicas/admin.py
+++ WorkAnt/FuncionesBasicas/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from FuncionesBasicas.models import DireccionEmpleadores, EmailRefetenceiaEmpleados , Empleadores , Empleados , TagBusqueda, TarjetaTrabajo , TelefonosEmpleadores , TelefonosEmpleados ,TiempoDisponible ,ZonaTrabajo, historialConsulta , historialCreacion 
+from FuncionesBasicas.models import DireccionEmpleadores, EmailRefetenceiaEmpleados , Empleadores , Empleados , TagBusqueda, TarjetaTrabajo , TelefonosEmpleadores , TelefonosEmpleados , historialConsulta , historialCreacion 
 
 admin.site.register(DireccionEmpleadores)
 admin.site.register(EmailRefetenceiaEmpleados)
@@ -10,8 +10,6 @@ admin.site.register(TagBusqueda)
 admin.site.register(TarjetaTrabajo)
 admin.site.register(TelefonosEmpleadores)
 admin.site.register(TelefonosEmpleados)
-admin.site.register(TiempoDisponible)
-admin.site.register(ZonaTrabajo)
 admin.site.register(historialConsulta)
 admin.site.register(historialCreacion)
 
---
+++ WorkAnt/FuncionesBasicas/llenadorBasico.py
@@ -0,0 +1,57 @@
+from FuncionesBasicas.models import DireccionEmpleadores, EmailRefetenceiaEmpleados , Empleadores , Empleados , TagBusqueda, TarjetaTrabajo , TelefonosEmpleadores , TelefonosEmpleados , historialConsulta , historialCreacion
+
+
+class llenadorTablas(self):
+
+    def llenadorTablasUsuario(self,pnombre, pcodigo, pfechaNac, pgenero, email, emailReferencia , ptelefonosEmpleados):
+        empleado=Empleados(nombre=pnombre, codigo=pcodigo, fechaNac=pfechaNac, genero = pgenero)
+        empleado.save()
+
+        for i in emailReferencia:
+            emailsR=EmailRefetenceiaEmpleados(llaveForanea=empleado, email=emailReferencia[i])
+            email.save()
+        
+        for i in ptelefonosEmpleados:
+            telfE=TelefonosEmpleados(llaveForanea=empleado,telf=ptelefonosEmpleados)
+            telfE.save()
+        
+    def llenadorTablasEmpleadores(self, pnombre, pcodigo, pemail, pdireccionEmpleadores, ptelefonosEmpleadores):
+        empleador=Empleadores(nombre=pnombre, codigo=pcodigo,emial=pemail)
+        empleador.save()
+
+        for i in pdireccionEmpleadores:
+            direc=DireccionEmpleadores(llaveForanea=empleador,direccion=pdireccionEmpleadores[i])
+            direc.save()
+
+        for i in ptelefonosEmpleadores:
+            telfE=TelefonosEmpleadores(llaveForanea=empleador, telf=ptelefonosEmpleadores[i])
+            telfE.save()
+
+    def llenadorTablasTarjetas(self, empleado_id , ptimempoDisponible,  pzona, ptagBusqueda):
+        empleado= Empleados.objects.get(id=empleado_id)
+
+        tarjeta=TarjetaTrabajo(llaveForanea=empleado,tiempoDisponible=ptimempoDisponible , zona=pzona)
+        tarjeta.save()
+
+        for i in ptagBusqueda:
+            ttag=TagBusqueda(llaveForanea=tarjeta,tag=ptagBusqueda[i])
+            ttag.save()
+
+    def llenadorTablasHistorialCreacion(self, tarjeta_id, empleado_id):
+        tarjeta=TarjetaTrabajo.objects.get(id=tarjeta_id)
+        empleado=Empleados.objects.get(id=empleado_id)
+    def llenadorTablasHistorialCreacion(self, tarjeta_id, empleado_id):
+        tarjeta=TarjetaTrabajo.objects.get(id=tarjeta_id)
+        empleado=Empleados.objects.get(id=empleado_id)
+
+        hcreacion=historialCreacion(llaveForaneaTarjeta=tarjeta, llaveForaneaEmpleados=empleado)
+        hcreacion.save()
+        hcreacion=historialCreacion(llaveForaneaTarjeta=tarjeta, llaveForaneaEmpleados=empleado)
+        hcreacion.save() 
+    
+    def llenadorTablasHistorialConsulta(self, tarjeta_id, empleador_id):
+        tarjeta=TarjetaTrabajo.objects.get(id=tarjeta_id)
+        empleador=Empleadores.objects.get(id=empleador_id)
+
+        hcreacion=historialConsulta(llaveForaneaTarjeta=tarjeta, llaveForaneaEmpleadores=empleador)
+        hcreacion.save()
